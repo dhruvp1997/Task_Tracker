19f3d99a1f25d0f817ec72b5478bb3e49d475a0c
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-06-16 16:57:55 -0400

    Till Toggle Reminder

---
+++ task_tracker/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button text="Add Task" color="green" />
+        
+    </header>
+</template>
+<!-- <Button text="Update Task" color="blue" />
+     <Button text="Delete Task" color="red" /> -->
+<script>
+import Button from './Button'
+    export default {
+        name: 'Header',
+        props: {
+            title: String
+            // title: {
+            //     type: String,
+            //     default: 'Hello World',
+            // }
+        },
+        components: {
+            Button
+        }
+    }
+</script>
+
+<style>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
